c399fb0d4b4a026d3146fb3916081a919986913d
Erik Soderstam <eriksoderstam@MBPsomtlhorErik.lan>
2022-02-11 21:06:39 +0100

    ejs added

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
+//imports
 const express = require('express')
 const app = express()
 const { ROLE, users } = require('./data')
 const { authUser, authRole } = require('./basicAuth')
 const projectRouter = require('./routes/projects')
+const { engine } = require('express/lib/application')
 
+
+//Set views
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+// Static files and more
+
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
 app.use(express.json())
 app.use(setUser)
 app.use('/projects', projectRouter)
 
+
 app.get('/', (req, res) => {
-  res.send('Home Page')
+  res.render('index', {text: 'This is EJS'})
 })
 
 app.get('/dashboard', authUser, (req, res) => {
-  res.send('Dashboard Page')
+  res.render('dashboard')
 })
 
 app.get('/admin', authUser, authRole(ROLE.ADMIN), (req, res) => {
-  res.send('Admin Page')
+  res.render('admin')
 })
 
 function setUser(req, res, next) {
